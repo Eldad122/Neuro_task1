640dbe006f6f68330d728703a66d35cadbef5f13
Eldad122 <61798552+Eldad122@users.noreply.github.com>
2021-04-26 14:54:13 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import numpy as np
 import random
 from sklearn.model_selection import train_test_split
-
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
 from AdalineAlgo import CustomAdaline
 
 
@@ -34,6 +35,29 @@ def data(data_part: int):
         y_data.append(temp_y)
     return np.array(x_data), np.array(y_data)
 
+def plot_decision_regions(X, y, classifier, resolution=0.02):
+    # setup marker generator and color map
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+
+    # plot the decision surface
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                            np.arange(x2_min, x2_max, resolution))
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    Z = Z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, Z, alpha=0.4, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    # plot class samples
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0], y=X[y == cl, 1],
+                    alpha=0.8, c=cmap(idx),
+                    marker=markers[idx], label=cl)
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     #
@@ -56,4 +80,10 @@ if __name__ == '__main__':
     # Score the model
     #
     print(adaline.score(X_test, y_test))
+    plot_decision_regions(X, y, classifier=adaline)
 
+    plt.title('Adaptive Linear Neuron - Gradient Descent')
+    plt.xlabel('sepal length [standardized]')
+    plt.ylabel('petal length [standardized]')
+    plt.legend(loc='upper left')
+    plt.show()
