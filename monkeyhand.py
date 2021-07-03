b6f1bc5f0d261f60a69030bef36f412956f4afce
Eldad122 <61798552+Eldad122@users.noreply.github.com>
2021-07-03 22:39:48 +0300

    Update monkeyhand.py

--- monkeyhand.py
+++ monkeyhand.py
@@ -17,7 +17,7 @@ def isInHand(x, y):  # 4 fingers
     return False
 
 
-def isInHand(x, y):  # 3 fingers
+def isIn3Hand(x, y):  # 3 fingers
     if y < 0.4:
         return True
     # elif 0 < x < 0.25 and y < 0.7:
@@ -32,9 +32,10 @@ def isInHand(x, y):  # 3 fingers
 
 
 def sample1():
-    x = np.random.uniform(0, 1, 50)
-    y = np.random.uniform(0, 1, 50)
-    for i in range(50):
+    size = 1000
+    x = np.random.uniform(0, 1, size)
+    y = np.random.uniform(0, 1, size)
+    for i in range(size):
         if not isInHand(x[i], y[i]):
             while not isInHand(x[i], y[i]):
                 x[i] = np.random.uniform(0, 1)
@@ -42,9 +43,29 @@ def sample1():
     step_max = 3000
     radius = 10
     alpha = 0.3
-    kohonen.draw(x, y, 1, radius, alpha)
+    kohonen.draw(x, y, 1, radius, alpha, is_line=False)
+    kohonen.train(x, y, step_max, radius, alpha)
+
+
+def sample2():
+    size = 1000
+    x = np.random.uniform(0, 1, size)
+    y = np.random.uniform(0, 1, size)
+    for i in range(size):
+        if not isIn3Hand(x[i], y[i]):
+            while not isIn3Hand(x[i], y[i]):
+                x[i] = np.random.uniform(0, 1)
+                y[i] = np.random.uniform(0, 1)
+    step_max = 3000
+    radius = 10
+    alpha = 0.3
+    kohonen.draw(x, y, 1, radius, alpha, is_line=False)
     kohonen.train(x, y, step_max, radius, alpha)
 
 
 if __name__ == '__main__':
-    sample1()
+    chosen = input("Choose a sample: ")
+    if chosen == '1':
+        sample1()
+    elif chosen == '2':
+        sample2()
